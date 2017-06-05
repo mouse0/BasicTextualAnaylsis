6a5c503c69833fa3d0acd08a0cec8f7013e41326
mouse0 <corey.oconnor10@gmail.com>
2017-06-05 15:30:20 -0600

    Sorry!
    Realized I forgot to uncomment the POS tagging code (I commented it out so I could run the whole file without needing to worry about speed, POS tagging is slow).

--- TextAnalyzer.R
+++ TextAnalyzer.R
@@ -86,60 +86,29 @@ wf <- read_csv(sprintf("%s/%s", base_url, "word_frequency.csv"))
 updatedTotalWF <- inner_join(totalWordsFreq, wf)
 filter(updatedTotalWF, frequency < 0.09)
 
-# Break up the corpus into smaller documents so they can be tagged
-# con <- file("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/masterDocument.txt")
-#tempDocument <- paste (readLines(con), collapse = "\n")
-#close(con)
-
-# segments <- strsplit(tempDocument, "\n")
-
-# temp <- ""
-#for (i in 1:length(segments[[1]])) {
-#  temp <- paste(temp, segments[[1]][[i]]) 
-#  if ((i %% 10 == 0) & (i != 0)) {
-#    fileName <- paste("file", i)
-#    fileName <- paste(fileName, ".txt", sep = "")
-#    path2 <- paste("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/", fileName, sep = "")
-#    con <- file(path2, "w")
-#    cat(temp, file = con)
-#    close(con)
-#    temp <- ""
-#  }
-#}
-# Use koRpus to find parts of speech to find out incidence 
-# of negation and coordination
-# files <- list.files(path = "~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/", pattern = "*.txt")
-# taggedDF = data_frame()
-# for (i in 1:length(files)) {
-#  print("Now tagging:")
-#  print(files[i])
-#  path <-paste("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/", files[i], sep = "")
-#  tagged <- treetag(path, treetagger = "manual", lang = "en", TT.options = list(path = "~/Code/tree-tagger-MacOSX-3.2", preset = "en"))
-#  taggedDF <- rbind(taggedDF, taggedText(tagged))
-#}
 
 # Use phrasemachine to tag the documents
-#files <- list.files("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/")
-#documents <- c(1:length(files))
-#for (i in 1:length(files)) {
-#  path <- paste("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/", files[i], sep = "")
-#  con <- file(path)
-#  documents[i] = readLines(con)
-#  close(con)
-#}
-#tagged <- POS_tag_documents(documents)
+files <- list.files("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/")
+documents <- c(1:length(files))
+for (i in 1:length(files)) {
+  path <- paste("~/Code/BasicTextualAnalysis/2017 Summer Fellowship data/Small Documents/", files[i], sep = "")
+  con <- file(path)
+  documents[i] = readLines(con)
+  close(con)
+}
+tagged <- POS_tag_documents(documents)
 
-#Write the messy list into one nice csv for safekeeping
+# Write the messy list into one nice csv for safekeeping
 
-#lapply(tagged, function(x) write.table(data.frame(x), "labeled.csv", append = T, sep = ","))
+lapply(tagged, function(x) write.table(data.frame(x), "labeled.csv", append = T, sep = ","))
 
 # Get it back as a nice data frame
-#taggedData <- read.csv("~/Code/BasicTextualAnalysis/labeled.csv")
+taggedData <- read.csv("~/Code/BasicTextualAnalysis/labeled.csv")
 
 # count the conjunctions
-#conjunctions = 0
-#for (i in 1:nrow(taggedData)) {
-#  if (taggedData[i, 3] == "CC") {
-#    conjunctions <- conjunctions + 1
-#  }
-#}
+conjunctions = 0
+for (i in 1:nrow(taggedData)) {
+  if (taggedData[i, 3] == "CC") {
+    conjunctions <- conjunctions + 1
+  }
+}
